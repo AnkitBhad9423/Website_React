a15a421f67a137eab539a455812edf4e9d9efe9f
ankitbhad <ankitbhad90223@gmail.com>
2024-08-10 23:49:07 +0530

    Website is all set including resposiveness

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import vg from "../assets/2.webp";
+import {
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
 const Home = () => {
   return (
     <>
-      <div className="home">
+      <div className="home" id="home">
         <main>
           <h1>Tech</h1>
           <p>Solution to all your problems</p>
@@ -20,7 +26,7 @@ const Home = () => {
           </p>
         </div>
       </div>
-      <div className="home3">
+      <div className="home3" id="about">
         <div>
           <h1>Who we are</h1>
           <p>
@@ -32,6 +38,48 @@ const Home = () => {
             aperiam, asperiores neque id soluta, accusamus animi sunt vero quae
             magni facilis alias. Maxime ad soluta autem!
           </p>
+        </div>
+      </div>
+      <div className="home4" id="brands">
+        <div>
+          <h1>Brands</h1>
+          <article>
+            <div
+              style={{
+                animationDelay: "0.3s",
+              }}
+            >
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.5s",
+              }}
+            >
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.7s",
+              }}
+            >
+              <AiFillYoutube />
+              <p>YouTube</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "1s",
+              }}
+            >
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+          </article>
         </div>
       </div>
     </>
